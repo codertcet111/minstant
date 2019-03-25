6fd5063cab6965c06591b163493c67cf90870f3c
shubham mishra <shubham.mishra@mirraw.com>
2019-03-25 19:07:20 +0530

    before Follow Following UI

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
   def create
     Post.create(post_params)
     redirect_to root_path
